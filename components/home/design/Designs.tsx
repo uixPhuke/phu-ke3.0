05077c35d29dd65b6f97f46d4ee5e3f99715cfa0
uixPhuke <uixphuke@gmail.com>
2025-04-01 23:38:23 +0530

    one image src change

--- components/home/design/Designs.tsx
+++ components/home/design/Designs.tsx
@@ -612,7 +612,7 @@ const designs = [
   },
   {
     title: "STReT x GRAFiTi",
-    imgSrc: "Full_20collection_20-_20Street_20signs_20-_20I_E2_80_99ve_20put_20it_20in_20the_20louvre_20_20_20Really_20love_20these_20mock-ups_2C_20what_20do_20you_20think_20guys_20And_20let_20me_20know_20fav_20sign_2C_20just_20put_20the_20number_20in_20the_-2_kiwsbl",
+    imgSrc: "Full_20collection_20-_20Street_20signs_20-_20I_E2_80_99ve_20put_20it_20in_20the_20louvre_20_20_20Really_20love_20these_20mock-ups_2C_20what_20do_20you_20think_20guys_20And_20let_20me_20know_20fav_20sign_2C_20just_20put_20the_20number_20in_20the_-7_pwvvrc",
 
     tech: ["illustrator"],
     description: "Chromic effect ",
